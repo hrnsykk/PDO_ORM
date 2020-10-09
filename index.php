5a62f8528a8c654293f3b876cf5ca876a19b689d
Harun Soykok <h.soykok@gmail.com>
2020-10-09 15:26:08 +0200

    Database instance in index.php

--- index.php
+++ index.php
@@ -2,4 +2,20 @@
 
 require_once("db.php");
 
-$db = new db();
+$db = new database();
+$data =  $db->table('urunler')->select('*')->limit([0,5])->order_by('isim')->run();
+//$data =  $db->table('urunler')->select('*')->where(['id' => 5])->run();
+//$insert = $db->table('urunler')->insert(['isim'=> 'Apple X' , 'resim' => '3.jpg' , 'fiyat' => '999.00'])->run();
+
+var_dump($data);
+
+
+$update = $db->table('urunler')
+            ->where(['id' => '13'])
+            ->update([
+                    ['isim' => 'Samsug Galaxy'],
+                    ['resim' => 'Samsung.jpg'],
+                    ['fiyat' => 555]
+                ])
+            ->run();
+
